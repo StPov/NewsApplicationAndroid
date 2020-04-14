c579eeadf8cbff2c20e40c6a3aba64749f41c194
Stanislav <staspovolotskiy@gmail.com>
2020-04-14 14:29:47 +0300

    init + Bottom Navigation

---
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.newsapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.sources:
+                        fragment = new SourcesFragment();
+                        break;
+                    case R.id.news:
+                        fragment = new NewsFragment();
+                        break;
+                    case R.id.favorites:
+                        fragment = new FavoritesFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SourcesFragment()).commit();
+        }
+    }
+}
